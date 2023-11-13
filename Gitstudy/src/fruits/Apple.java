c5744c135bcb479331e398bfc9ff2392900de4ef
Gi7E-36 <Gi7E-36@Gi7A-03>
2023-11-13 14:00:19 +0900

    add setter to apple

--- Gitstudy/src/fruits/Apple.java
+++ Gitstudy/src/fruits/Apple.java
@@ -2,4 +2,21 @@ package fruits;
 
 public class Apple {
 	int price;
+	int quality;
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public int getQuality() {
+		return quality;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
 }
